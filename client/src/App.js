2689fbead85ca86519ae28f0be02206d5a8cefc8
wisdomtooth <cbtsao47@gmail.com>
2018-12-30 23:39:49 -0600

    add logo img

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import KH3Logo from "./images/Kingdom_Hearts_III_Logo.png";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="logo__wrapper">
+          <img src={KH3Logo} alt="logo" className="logo__img" />
+        </div>
       </div>
     );
   }
